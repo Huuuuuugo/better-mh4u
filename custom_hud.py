1399bf4de6ff0855f6622e3a3e37dfc84cf4e5de
Hugo Messias <hugomessias9@gmail.com>
2024-08-10 13:24:41 -0300

    Added option to alter refresh rate.
    Turned 'tk_window' into a class attribute and added a more reliable way to set its value.
    
    Added function to monitor CPU usage.

--- custom_hud.py
+++ custom_hud.py
@@ -1,18 +1,29 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageTk
+import tkinter as tk
+import threading
 import time
 
 import pygetwindow as gw
 import numpy as np
+import psutil
 import cv2
 
-from window_utils import *
+from window_utils import get_citra_window, background_screenshot
 
-KEEP_CLOCK_ON_HUD = False
-HIDE_HUD_DELAY = 2
-HUD_UPDATE_DELAY = 100
+#TODO: end program when citra window is closed
+#TODO: check for changes on the weapon info section along with health and stamina
+#TODO: make it support different screen resolutions
+
+# customizable properties
+KEEP_CLOCK_ON_HUD = False   # show quest clock on HUD overlay
+HIDE_HUD_DELAY = 2          # seconds untill the HUD is hidden due to inactivity
+HUD_FPS = 20                # refresh rate of the HUD overlay, a higher refresh rate means a higher CPU use
+
+# constants
+HUD_FRAME_TIME = 1/HUD_FPS
 
 
-def has_color(image: Image, color, tolerance, section: tuple | None = None):
+def has_color(image: Image, color: tuple[int, int, int], tolerance: int, section: tuple | None = None):
     # convert image to RGBA
     if image.mode != "RGBA":
         image = image.convert("RGBA")
@@ -64,30 +75,36 @@ def remove_color(image: Image, color, tolerance):
 
 
 class DisplayImage():
-    p1_color = (180, 30, 15) # red
+    # colors that identify if the HUD is on screen
+    p1_combat_color = (180, 30, 15) # red
     p2_color = (50, 175, 175) # cyan
-    p3_color = (235, 230, 10) # yellow
+    p3_spotted_color = (235, 230, 10) # yellow
     p4_color = (10, 205, 10) # green
+    frenzy_color = (175, 80, 210) #purple
 
+    # attributes related to the HUD overlay window
     toggle_tk_window = False
+    tk_window: gw.Win32Window | None = None
 
+    # selecting the base mask to include the quest clock or not
     if KEEP_CLOCK_ON_HUD:
         mask = Image.open("__status_mask_clock.png")
     else:
         mask = Image.open("__status_mask_clean.png")
 
+    # declaration of other needed attributes
     prev_health_stamina_img = np.array(mask)[10: 10+20, 100:]
     prev_health_stamina_img = prev_health_stamina_img[:, :, :3]
-    
     hud_timer = 0
 
     @classmethod
     def check_for_player(cls, image):
         # checks if any of the player colors is found within the section that shows the weapon
-        return (has_color(image, cls.p1_color, 30, (100, 10, 30, 30)) or 
+        return (has_color(image, cls.p1_combat_color, 30, (100, 10, 30, 30)) or 
                 has_color(image, cls.p2_color, 30, (100, 10, 30, 30)) or
-                has_color(image, cls.p3_color, 30, (100, 10, 30, 30)) or
-                has_color(image, cls.p4_color, 30, (100, 10, 30, 30)))
+                has_color(image, cls.p3_spotted_color, 30, (100, 10, 30, 30)) or
+                has_color(image, cls.p4_color, 30, (100, 10, 30, 30)) or
+                has_color(image, cls.frenzy_color, 30, (100, 10, 30, 30)))
 
     @classmethod
     def start(cls, window):
@@ -115,8 +132,8 @@ class DisplayImage():
             return image
         
         def update_image():
-            # test performance
-            time_start = time.perf_counter()
+            # start frame timer
+            frame_timer_start = time.perf_counter()
 
             # get image
             image = get_image()
@@ -134,30 +151,26 @@ class DisplayImage():
                 cls.prev_health_stamina_img = health_stamina_img
 
                 # if the difference is grater than a given threshold
-                if difference > 5:
+                if difference > 0:
                     # restart timer
                     cls.hud_timer = time.perf_counter()
 
                     # get window back into view
                     if cls.toggle_tk_window:
-                        tk_window: gw.Win32Window = gw.getWindowsWithTitle("Display Image")
-                        if tk_window:
-                            tk_window = tk_window[0]
-                            top = 605 - tk_window.top
-                            left = 905 - tk_window.left
-                            tk_window.move(left, top)
+                        if cls.tk_window is not None:
+                            top = 605 - cls.tk_window.top
+                            left = 905 - cls.tk_window.left
+                            cls.tk_window.move(left, top)
                             cls.toggle_tk_window = False
                 
                 # if there's no significant difference and HIDE_HUD_DELAY has be reached
                 elif (time.perf_counter() - cls.hud_timer) >= HIDE_HUD_DELAY:
                     # hide window
                     if not cls.toggle_tk_window:
-                        tk_window: gw.Win32Window = gw.getWindowsWithTitle("Display Image")
-                        if tk_window:
-                            tk_window = tk_window[0]
-                            top = 1080 - tk_window.top
-                            left = 1920 - tk_window.left
-                            tk_window.move(left, top)
+                        if cls.tk_window is not None:
+                            top = 1080 - cls.tk_window.top
+                            left = 1920 - cls.tk_window.left
+                            cls.tk_window.move(left, top)
                             cls.toggle_tk_window = True
                 
                 # remove unnecessary portions of the image
@@ -177,19 +190,21 @@ class DisplayImage():
             else:
                 # hide window
                 if not cls.toggle_tk_window:
-                    tk_window: gw.Win32Window = gw.getWindowsWithTitle("Display Image")
-                    if tk_window:
-                        tk_window = tk_window[0]
-                        top = 1080 - tk_window.top
-                        left = 1920 - tk_window.left
-                        tk_window.move(left, top)
+                    if cls.tk_window is not None:
+                        top = 1080 - cls.tk_window.top
+                        left = 1920 - cls.tk_window.left
+                        cls.tk_window.move(left, top)
                         cls.toggle_tk_window = True
-            
-            # test performance
-            time_end = time.perf_counter() - time_start
-            print(f"processing time: {time_end}")
 
-            label.after(HUD_UPDATE_DELAY, update_image)
+            # wait untill frame timer reaches the frame time of the HUD refresh rate
+            frame_timer_end = time.perf_counter() - frame_timer_start
+            # print(f"\rprocessing time: {round(frame_timer_end, 5):.5f}", end="")
+            while frame_timer_end < HUD_FRAME_TIME:
+                time.sleep(0.0001)
+                frame_timer_end = time.perf_counter() - frame_timer_start
+            # print(f" | final time: {round(frame_timer_end, 5):.5f}", end="")
+
+            label.after(1, update_image)
 
         # Create a new Tkinter window
         root = tk.Tk()
@@ -208,6 +223,15 @@ class DisplayImage():
 
         update_image()
 
+        def get_tk_window():
+            time.sleep(0.5)
+            windows_list = []
+            while not windows_list:
+                windows_list = gw.getWindowsWithTitle("Display Image")
+
+            cls.tk_window = windows_list[0]
+        threading.Thread(target=get_tk_window, args=(), daemon=True).start()
+
         root.mainloop()
 
 if __name__ == "__main__":
@@ -225,8 +249,35 @@ if __name__ == "__main__":
                 return
             except Exception as e:
                 print(e)
-    
-    # threading.Thread(target=debug, args=(), daemon=True).start()
+
+    def get_cpu_usage(interval=1):
+        process = psutil.Process()
+        time.sleep(3)
+        usage_list = []
+        max_usage_list_len = 10
+        cpu_high = 0
+        avg_usage = 0
+        avg_high = 0
+
+        while True:
+            # Get CPU usage percentage of the process over the specified interval
+            cpu_usage = process.cpu_percent(interval=interval)
+            if cpu_usage > cpu_high:
+                cpu_high = cpu_usage
+
+            usage_list.append(cpu_usage)
+            if len(usage_list) > max_usage_list_len:
+                usage_list.pop(0)
+
+            avg_usage = (sum(usage_list)/len(usage_list))
+            if avg_usage > avg_high and len(usage_list) == max_usage_list_len:
+                avg_high = avg_usage
+            
+            # Print the CPU usage
+            print(f"CPU usage: {cpu_usage:.2f}% | average usage: {avg_usage:.2f}% | CPU high: {cpu_high:.2f}% | average high: {avg_high:.2f}%    \r", end='')
+
+    # threading.Thread(target=debug, args=(), daemon=True).start()              # start debug thread
+    # threading.Thread(target=get_cpu_usage, args=(0.5,), daemon=True).start()  # start cpu usage thread
 
     # get window
     window = get_citra_window("Janela Secundária")
